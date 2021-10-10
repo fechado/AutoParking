9cc02d78ad534734cfbc8db070aa3dedbd74f654
megavas <megavas@outlook.com>
2021-10-10 17:34:59 -0500

    simple

--- app.py
+++ app.py
@@ -1,18 +1,11 @@
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, redirect, url_for, session, jsonify
-from flask.helpers import url_for
-from config_lang import ConfigLang
-
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-langIni = 'lang_ESP.ini'
-
 @app.route("/")
 def index():
-    lang = ConfigLang(langIni, 'LOGIN')
-    return render_template('index.html', lang=lang)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=False)
